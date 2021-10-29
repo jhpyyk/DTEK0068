5f9e724b91534707488eff6a2931af4ea6f4a2ec
Juuso Pyykkönen <jhpyyk@utu.fi>
2021-10-29 21:29:29 +0300

    Add W01E01

---
+++ W0E01_PushLED.X/main.c
@@ -0,0 +1,27 @@
+/*
+ * File:   main.c
+ * Author: Juuso Pyykkönen
+ *
+ * Created on October 29, 2021, 9:01 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    
+    PORTF.DIR |= PIN5_bm;
+    PORTF.DIR &= ~PIN6_bm;
+    
+    while (1)
+    {
+        if(PORTF.IN & PIN6_bm)
+        {
+            PORTF.OUT |= PIN5_bm;
+        }
+        else
+        {
+            PORTF.OUT &= ~PIN5_bm; 
+        }
+    }
+}
